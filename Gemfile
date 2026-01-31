2857564a85278b494eff484b33285b27974dcaa7
viet247 <viet24799@gmail.com>
2026-01-31 17:32:53 +0700

    feat: authen user

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source "https://rubygems.org"
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
+ruby "3.3.0"
 gem "rails", "~> 8.0.4"
 # Use mysql as the database for Active Record
 gem "mysql2", "~> 0.5"
--- db/migrate/20260130083706_rename_password_digest_to_encrypted_password_in_users.rb
+++ db/migrate/20260130083706_rename_password_digest_to_encrypted_password_in_users.rb
@@ -1,5 +1,5 @@
 class RenamePasswordDigestToEncryptedPasswordInUsers < ActiveRecord::Migration[8.0]
   def change
-    rename_column :users, :password_digest, :encrpyted_passwrod
+    rename_column :users, :password_digest, :encrypted_password
   end
 end
