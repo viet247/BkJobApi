7155c4593988e66643993f699aa6d3e405d79f13
viet247 <viet24799@gmail.com>
2026-03-10 11:13:44 +0700

    feat: request spec for detail job

--- spec/requests/api/v1/jobs_spec.rb
+++ spec/requests/api/v1/jobs_spec.rb
@@ -6,19 +6,20 @@ RSpec.describe "Api::V1::Jobs", type: :request do
   let(:common_industry) { create(:industry) }
   let(:common_city)     { create(:city) }
 
-  let!(:jobs) do 
-    create_list(:job, 25, 
-      company: common_company, 
-      industry: common_industry, 
+  let!(:jobs) do
+    create_list(:job, 25,
+      company: common_company,
+      user: common_user,
+      industry: common_industry,
       city: common_city
-    ) 
+    )
   end
   describe "GET /api/v1/jobs" do
     # Happy path
     context "Correct Param" do
       it 'Return status code 200 and 20 first record ' do
-        get '/api/v1/jobs', params: {page: 1}
-  
+        get '/api/v1/jobs', params: { page: 1 }
+
         expect(response).to have_http_status(:ok)
         json_response = JSON.parse(response.body)
         expect(json_response['data'].size).to eq(20)
@@ -29,22 +30,44 @@ RSpec.describe "Api::V1::Jobs", type: :request do
     context "Param page is string(abc)" do
       it "Auto return data of page 1" do
         get "/api/v1/jobs", params: { page: "abc" }
-        
+
         expect(response).to have_http_status(:ok)
         json = JSON.parse(response.body)
-        
+
         # Check 20 record of page 1 or not
         expect(json["data"].size).to eq(20)
-      end 
+      end
     end
 
     context "Param page is negative (-1)" do
       it "Auto rerturn data of page 1" do
         get "/api/v1/jobs", params: { page: -1 }
-        
+
         expect(response).to have_http_status(:ok)
         json = JSON.parse(response.body)
         expect(json["data"].size).to eq(20)
+      end
+    end
+  end
+
+  describe "Get /api/v1/jobs/:id" do
+    # Happy path
+    context "Job is exists" do
+      it "Return status code 200" do
+        get "/api/v1/jobs/#{jobs.first.id}"
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    # Sad path
+    context "Job is not exists" do
+      before { get "/api/v1/jobs/-1" }
+      it "Return status code 404" do
+        expect(response).to have_http_status(:not_found)
+      end
+      it "Return right message in body" do
+        json = JSON.parse(response.body)
+        expect(json['message']).to eq("Job not found!")
       end
     end
   end
