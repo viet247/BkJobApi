4aacac948964cb016f15992f7d23e3ff29c04b64
viet247 <viet24799@gmail.com>
2026-03-13 17:01:27 +0700

    feat: add route for create job

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :jobs, only: [ :index, :show ]
+      resources :jobs, only: [ :index, :show, :create ]
     end
   end
 end
